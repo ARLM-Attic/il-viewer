661bd7f38b406c352e89e13b3beec3bc48bfcb7c
SND\LogicMiner_cp <SND\LogicMiner_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2011-01-19 15:52:03 +0000

    Made the addin compatible for VS 2010 also
    git-svn-id: https://ilviewer.svn.codeplex.com/svn@83610 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ ILViewer/CommitActions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Win32;
+using System.IO;
+using System.Diagnostics;
+
+namespace CommitActions
+{
+    class Program
+    {
+        internal static List<string> VSExeFolders;
+
+        static void Main(string[] args)
+        {
+            //load visual studio exe locations           
+            RegistryKey visualStudioRootReg = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\VisualStudio");
+            VSExeFolders = visualStudioRootReg.GetSubKeyNames().ToList().Where(i => visualStudioRootReg.OpenSubKey(i).GetValue("InstallDir") != null).Select(i => Convert.ToString(visualStudioRootReg.OpenSubKey(i).GetValue("InstallDir"))).ToList();
+            VSExeFolders = VSExeFolders.Where(i => File.Exists(i.Trim('\\') + "\\" + "devenv.exe")).Select(i => i.Trim('\\') + "\\" + "devenv.exe").ToList();
+            VSExeFolders.Distinct().ToList().ForEach(
+                i =>
+                {
+                    Process process = new Process();
+                    process.StartInfo = new ProcessStartInfo(i, "/resetaddin ILViewer.Connect");
+                    process.Start();
+                }
+                );
+        }
+
+    }
+}
